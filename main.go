6b4b136653c2c9e49864e13c57a5891d72d4a48d
Nick Adams <nick@cortexdigitalinc.com>
2024-04-15 21:30:15 -0400

    moved env secrets into secrets, wrote encryption and decryption

--- main.go
+++ main.go
@@ -1,124 +1,142 @@
 package main
 
 import (
+	"crypto/aes"
+	"crypto/cipher"
+	"encoding/base64"
 	"fmt"
 	"html/template"
 	"log/slog"
 	"net/http"
 	"net/url"
+	"os"
 
 	sendgrid "github.com/sendgrid/sendgrid-go"
 	"github.com/sendgrid/sendgrid-go/helpers/mail"
 )
 
 type header struct {
-    Title string
+	Title string
 }
 
 type footer struct {
-    Year string 
-}   
-
-type pageData struct {
-    Header header
-    Content interface{}
-    Footer footer
+	Year string
 }
 
-var templates map[string]*template.Template 
+type pageData struct {
+	Header  header
+	Content interface{}
+	Footer  footer
+}
+
+var templates map[string]*template.Template
 
 func main() {
 
-    var err error
-    
-    templateDefs := []string{"home", "contact"}
-    templates = make(map[string]*template.Template)
+	email := os.Getenv("DEV_EMAIL")
+	fmt.Println(email)
+	var err error
 
-    // parse html
-    for _, name := range templateDefs {
-        if templates[name] == nil {
-            templates[name] = template.New(name)
-            templates[name] = getTemplate(templates[name], "views/parts/page.html", "views/"+name+".html")
-        }
-    }
+	encryptedEmail, err := encrypt(email)
+	if err != nil {
+		fmt.Println("unable to encrypt email", err)
+	}
+	fmt.Println(encryptedEmail)
 
-    // routes
-    http.HandleFunc("/", Home)
-    http.HandleFunc("/contact", Contact)
-    http.HandleFunc("/contactSubmit", ContactSubmit)
+	decryptedEmail, err := decrypt(encryptedEmail)
+	if err != nil {
+		fmt.Println("unable to decrypt email", err)
+	}
+	fmt.Println(decryptedEmail)
 
-    http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("views/assets/"))))
+	templateDefs := []string{"home", "contact"}
+	templates = make(map[string]*template.Template)
 
-    err = http.ListenAndServe(":3000", nil)
-    
-    if err != nil {
-        slog.Debug("error starting server", err)
-        panic(err)
-    }
+	// parse html
+	for _, name := range templateDefs {
+		if templates[name] == nil {
+			templates[name] = template.New(name)
+			templates[name] = getTemplate(templates[name], "views/parts/page.html", "views/"+name+".html")
+		}
+	}
 
-    fmt.Println("********** Server Ready **********")
+	// routes
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/contact", Contact)
+	http.HandleFunc("/contactSubmit", ContactSubmit)
+
+	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("views/assets/"))))
+
+	err = http.ListenAndServe(":3000", nil)
+
+	if err != nil {
+		slog.Debug("error starting server", err)
+		panic(err)
+	}
+
+	fmt.Println("********** Server Ready **********")
 
 }
 
 func Render(w http.ResponseWriter, pageTemplate *template.Template, data any) {
-    err := pageTemplate.ExecuteTemplate(w, "page", data)
-    if err != nil {
-        slog.Debug("error executing template", err)
-        panic(err)
-    }   
+	err := pageTemplate.ExecuteTemplate(w, "page", data)
+	if err != nil {
+		slog.Debug("error executing template", err)
+		panic(err)
+	}
 }
 
 func Home(w http.ResponseWriter, r *http.Request) {
-    fmt.Println("in home")
+	fmt.Println("in home")
 
-    data := getPageData()
+	data := getPageData()
 
-    Render(w, templates["home"], data)
+	Render(w, templates["home"], data)
 }
 
 func Contact(w http.ResponseWriter, r *http.Request) {
-    Render(w, templates["contact"], nil)
+	Render(w, templates["contact"], nil)
 }
 
 func ContactSubmit(w http.ResponseWriter, r *http.Request) {
-    err := r.ParseForm()
-    if err != nil {
-        slog.Debug("error parsing form data", err)
-        panic(err)
-    }
+	err := r.ParseForm()
+	if err != nil {
+		slog.Debug("error parsing form data", err)
+		panic(err)
+	}
 
-    fmt.Println("name: ", r.Form["name"])
-    fmt.Println("email: ", r.Form["email"])
-    fmt.Println("message: ", r.Form["message"])
+	fmt.Println("name: ", r.Form["name"])
+	fmt.Println("email: ", r.Form["email"])
+	fmt.Println("message: ", r.Form["message"])
 
-    fmt.Println("in contact submit")
-    sendMail(r.Form)
+	fmt.Println("in contact submit")
+	sendMail(r.Form)
 
-    http.ServeFile(w, r, "views/parts/contact-form.html")
+	http.ServeFile(w, r, "views/parts/contact-form.html")
 }
 
 func getTemplate(t *template.Template, filePaths ...string) *template.Template {
-    templatePart, err := t.ParseFiles(filePaths...)
-    if err != nil {
-        slog.Debug("error parsing template file", err)
-        panic(err)
-    }
-    
-    return templatePart  
+	templatePart, err := t.ParseFiles(filePaths...)
+	if err != nil {
+		slog.Debug("error parsing template file", err)
+		panic(err)
+	}
+
+	return templatePart
 }
 
 func getPageData() *pageData {
-    header := header{Title: "title"}
-    content := struct{HTML string}{HTML: `<p>this is my first htmx site.</p>`}
-    footer := footer{Year: "2024"}
+	header := header{Title: "title"}
+	content := struct{ HTML string }{HTML: `<p>this is my first htmx site.</p>`}
+	footer := footer{Year: "2024"}
 
-    data := pageData{Header: header, Content: content, Footer: footer}
+	data := pageData{Header: header, Content: content, Footer: footer}
 
-    return &data
+	return &data
 }
 
 func sendMail(form url.Values) {
-    from := mail.NewEmail("Example User", "test@example.com")
+	from := mail.NewEmail("Example User", "test@example.com")
 	subject := "Sending with Twilio SendGrid is Fun"
 	to := mail.NewEmail("Example User", "")
 	plainTextContent := "and easy to do anywhere, even with Go"
@@ -127,7 +145,7 @@ func sendMail(form url.Values) {
 	client := sendgrid.NewSendClient("")
 	response, err := client.Send(message)
 	if err != nil {
-        slog.Debug("error sending mail", err)
+		slog.Debug("error sending mail", err)
 	} else {
 		fmt.Println(response.StatusCode)
 		fmt.Println(response.Body)
@@ -135,3 +153,42 @@ func sendMail(form url.Values) {
 	}
 }
 
+func encrypt(secret string) (string, error) {
+
+	obfuscation := []byte{93, 02, 10, 94, 10, 48, 30, 02, 93, 92, 50, 19, 30, 93, 01, 00}
+	const secretKey = "iwojsdfniawojksdf0-02#$234SDf2#$"
+
+	block, err := aes.NewCipher([]byte(secretKey))
+	if err != nil {
+		return "", err
+	}
+
+	secretBytes := []byte(secret)
+	cfb := cipher.NewCFBEncrypter(block, obfuscation)
+	xorLayer := make([]byte, len(secretBytes))
+	cfb.XORKeyStream(xorLayer, secretBytes)
+	return base64.StdEncoding.EncodeToString(xorLayer), nil
+}
+
+func decrypt(classified string) (string, error) {
+
+	obfuscation := []byte{93, 02, 10, 94, 10, 48, 30, 02, 93, 92, 50, 19, 30, 93, 01, 00}
+	const secretKey = "iwojsdfniawojksdf0-02#$234SDf2#$"
+
+	block, err := aes.NewCipher([]byte(secretKey))
+	if err != nil {
+		return "", err
+	}
+
+	classifiedBytes, err := base64.StdEncoding.DecodeString(classified)
+	if err != nil {
+		return "", err
+	}
+
+	cfb := cipher.NewCFBDecrypter(block, obfuscation)
+	xorLayer := make([]byte, len(classifiedBytes))
+	cfb.XORKeyStream(xorLayer, classifiedBytes)
+
+	return string(xorLayer), nil
+
+}
